4e47fa686162ea550a545b2e2fbd175466af1efb
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-09 18:50:15 -0500

    add funny_string solution from HackerRank

---
+++ HakerRank/algorithms/strings/funny_string.cs
@@ -0,0 +1,36 @@
+/*
+author: Daniel Lozano
+source: HAckerRank(https://www.hackerrank.com )
+problem name: Algorithms > Strings > Funny String
+problem url: https://www.hackerrank.com/challenges/funny-string/problem
+*/
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int cases = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < cases; j++)
+            {
+                string S = Console.ReadLine();
+
+                bool funny = true;
+
+                for (int i = 1; i < S.Length && funny; i++)
+
+                    if (Math.Abs(S[i] - S[i - 1]) != Math.Abs(S[S.Length - i - 1] - S[S.Length - i - 1 + 1]))
+                        funny = false;
+
+                if (funny)
+                    Console.WriteLine("Funny");
+                else
+                    Console.WriteLine("Not Funny");
+            }
+        }
+    }
+}
---
+++ HakerRank/algorithms/strings/funny_string.py
@@ -0,0 +1,13 @@
+# author: Daniel Lozano
+# source: HAckerRank ( https://www.hackerrank.com )
+# problem name: Algorithms > Strings > Funny String
+# problem url: https://www.hackerrank.com/challenges/funny-string/problem
+
+for _ in range(int(input())):
+    s = list(map(ord, input()))
+    funny, i = True, 1
+    while i < len(s) and funny:
+        if abs(s[i] - s[i - 1]) != abs(s[-i - 1] - s[-i]):
+            funny = False
+        i += 1    
+    print("Funny" if funny else "Not Funny")
