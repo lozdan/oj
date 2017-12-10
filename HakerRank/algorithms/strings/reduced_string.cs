9a1f9e1f31dc760f74d57dc76331a65e940cde36
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-09 19:08:37 -0500

    add namespace and header comments to super_reduced_string.cs solution from HackerRank

--- HakerRank/algorithms/strings/reduced_string.cs
+++ HakerRank/algorithms/strings/reduced_string.cs
@@ -1,24 +1,35 @@
+/*
+author: Daniel Lozano
+source: HAckerRank(https://www.hackerrank.com )
+problem name: Algorithms > String > Super Reduced String
+problem url: https://www.hackerrank.com/challenges/reduced-string/problem
+*/
+
 using System;
 
-class Solution {
-    static void Main()
-    {            
-        string line = Console.ReadLine();
-
-        int count = 0;
-        while (count < line.Length - 1)
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main()
         {
-            if (line[count] == line[count + 1])
-            {
-                line = line.Remove(count, 2);
-                count = -1;
-            }
-            count++;
-        }
+            string line = Console.ReadLine();
 
-        if (String.IsNullOrEmpty(line))            
-            Console.Write("Empty String");            
-        else            
-            Console.Write(line);            
-    }                                       
+            int count = 0;
+            while (count < line.Length - 1)
+            {
+                if (line[count] == line[count + 1])
+                {
+                    line = line.Remove(count, 2);
+                    count = -1;
+                }
+                count++;
+            }
+
+            if (String.IsNullOrEmpty(line))
+                Console.Write("Empty String");
+            else
+                Console.Write(line);
+        }
+    }
 }
