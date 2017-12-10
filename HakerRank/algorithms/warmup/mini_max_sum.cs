61ec03674659e1b8e9227681f8b6f471368b0ea1
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-09 19:00:48 -0500

    fix min_max_sum.cs solution from HackerRank

--- HakerRank/algorithms/warmup/mini_max_sum.cs
+++ HakerRank/algorithms/warmup/mini_max_sum.cs
@@ -1,17 +1,28 @@
-//C#
+/*
+author: Daniel Lozano
+source: HAckerRank(https://www.hackerrank.com )
+problem name: Algorithms > Warmup > Mini-Max Sum
+problem url: https://www.hackerrank.com/challenges/mini-max-sum/problem
+*/
 
-static void Main(String[] args) 
+using System;
+using System.Linq;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main()
         {
-            string[] line_temp = Console.ReadLine().Split();
-            long[] line = Array.ConvertAll(line_temp, long.Parse);
-                     
+            long[] line = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+            
             Array.Sort(line);
-            
-            long array_sum = line.Sum();
-            long min_value = array_sum - line[4];
-            long max_value = array_sum - line[0];
 
-            Console.Write(min_value + " " + max_value);
-            }
-            
+            long arraySum = line.Sum();
+            long minValue = arraySum - line[4];
+            long maxValue = arraySum - line[0];
+
+            Console.Write(minValue + " " + maxValue);
         }
+    }
+}
