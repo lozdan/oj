41c7e601d8e44e64a4314a15f95f41d821f97023
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-14 22:48:09 -0500

    add solution for problem camelcase (HackerRank)

---
+++ HakerRank/algorithms/strings/camelcase.py
@@ -0,0 +1,14 @@
+/*
+author: Daniel Lozano
+source: HAckerRank(https://www.hackerrank.com )
+problem name: Algorithms > Strings > CamelCase
+problem url: https://www.hackerrank.com/challenges/camelcase/problem
+*/
+
+s = input().strip()
+
+words = 1
+for i in range(len(s)):
+    if s[i] == s[i].upper():
+        words += 1
+print(words)
