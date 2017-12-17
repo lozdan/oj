fdec2f76467f5db041075714432105567c9c3a85
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-17 02:35:47 -0500

    improve solution of metro_card problem (CodeFights)

--- CodeFights/Arcade/metro_card.py
+++ CodeFights/Arcade/metro_card.py
@@ -4,9 +4,7 @@
 # problem url: https://codefights.com/arcade/code-arcade/at-the-crossroads/J7PQDxpWqhx7HrvBZ
 
 def metroCard(lastNumberOfDays):
-    if lastNumberOfDays == 28:
-        return [31]
-    elif lastNumberOfDays == 30:
+    if lastNumberOfDays == 28 or lastNumberOfDays == 30:
         return [31]
     else:
         return [28, 30, 31]
