65c8f8c9ab22bdf79d2369ab26ff3bda6e491678
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-29 18:01:36 -0500

    improve solution for rangeDigitSum problem

--- CodeFights/Challenge/rangeDigitSum.py
+++ CodeFights/Challenge/rangeDigitSum.py
@@ -4,8 +4,14 @@
 # problem url: https://codefights.com/challenge/KJW7qMKX2ee375mPQ
 
 def rangeDigitSum(A, B):
-    z = 0
+    total = 0
     for i in range(A, B + 1):
-        for j in range(len(str(i))):
-            z = z + int(str(i)[j])
-    return z
+        total += digits_sum(i)
+    return total
+        
+def digits_sum(value):
+    res = 0
+    while value != 0:
+            res += value % 10
+            value = value // 10
+    return res
