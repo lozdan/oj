0c04b8cccbc520835f29abccfc64b2bc5bcaad4a
Daniel Lozano <lozanodaniel02@gmail.com>
2017-12-29 19:21:36 -0500

    improve solution for primeCount problem (Codefights)

--- CodeFights/Challenge/primeCount.py
+++ CodeFights/Challenge/primeCount.py
@@ -4,11 +4,19 @@
 # problem url: https://codefights.com/challenge/WLCubrLrQ9grg7S8x
 
 def primeCount(N):
-    l = set()
+    if N == 1:
+        return 0
+    numbers = [0] * N
+    numbers[0], numbers[1] = 1, 1
+    total = 2
     count = 0
-    for i in range(2, N):
-        if i not in l:
-            count += 1
-            l.update(range(i * i, N, i))
-    return count
+    while count ** 2 < N:
+        if numbers[count] == 0:
+            for i in range(count * count, N, count):
+                if numbers[i] == 0:
+                    numbers[i] = 1
+                    total += 1
+        count += 1
+    
+    return N - total
             
