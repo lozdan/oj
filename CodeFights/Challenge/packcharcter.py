f94175ee5d839359cfb23c421f7f86c4f1d25988
Daniel Lozano <lozanodaniel02@gmail.com>
2018-01-02 23:24:50 -0500

    add solution for packcharcter problem (CodeFights)

---
+++ CodeFights/Challenge/packcharcter.py
@@ -0,0 +1,7 @@
+# author: Daniel Lozano
+# source: CodeFights ( https://codefights.com )
+# problem name: Challenge > packcharcter
+# problem url: https://codefights.com/challenge/R6uCL49kw44stKbM2
+
+def packcharcter(X, Y):
+    return int("{}{}".format(ord(X), ord(Y)))
