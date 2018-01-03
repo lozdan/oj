19373d0e08c90447c0934459aa497c0bb2a75e03
Daniel Lozano <lozanodaniel02@gmail.com>
2018-01-03 03:04:29 -0500

    add solution for findNthSmallestMultipleInArray problem (CodeFights)

---
+++ CodeFights/Challenge/findNthSmallestMultipleInArray.py
@@ -0,0 +1,7 @@
+# author: Daniel Lozano
+# source: CodeFights ( https://codefights.com )
+# problem name: Challenge > findNthSmallestMultipleInArray
+# problem url: https://codefights.com/challenge/GrnTbWrj62B6AJXag
+
+def findNthSmallestMultipleInArray(array, nth):
+    return min(array) * nth
