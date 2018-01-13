8c5031db56a18a0c8484f430f6db5321f4dcb4b3
Daniel Lozano <lozanodaniel02@gmail.com>
2018-01-13 18:36:14 -0500

    Style Change

--- CodeFights/Challenge/bestFriendNames.cs
+++ CodeFights/Challenge/bestFriendNames.cs
@@ -15,22 +15,15 @@ namespace CodeFigths
 
         static bool bestFriendNames(string name1, string name2)
         {
-            if (sumCal(name1.ToLower().Replace(" ", "")) == sumCal(name2.ToLower().Replace(" ", "")))
-            {
-                return true;
-            }
-            return false;
+            return sumCal(name1.ToLower().Replace(" ", "")) == sumCal(name2.ToLower().Replace(" ", ""));
         }
 
         static int sumCal(string name)
         {
             int total = 0;
             for (int i = 0; i < name.Length; i++)
-            {
                 total += name[i] - 96;
-            }
             return total;
         }
-
     }
 }
