01c3f9df484b9e9691f93f4d22f63122c5727ece
Daniel Lozano <lozanodaniel02@gmail.com>
2018-02-18 03:28:25 -0500

    add solution for reverse_integer problem (LeetCode)

---
+++ LeetCode/reverse_integer.py
@@ -0,0 +1,25 @@
+# author: Daniel Lozano
+# source: LeetCode ( https://www.leetcode.com )
+# problem name: Problems > Reverse Integer
+# problem url: https://leetcode.com/problems/reverse-integer/description/
+
+class Solution:
+    def reverse(self, x):
+        if x > 0:
+            answer = self.reverse_1(x)
+            if answer < 2 ** 31 - 1:
+                return answer
+            return 0
+        else:
+            answer = self.reverse_1(-x) * -1
+            if answer > -2 ** 31:
+                return answer
+            return 0
+
+    def reverse_1(self, x):
+        y = 0
+        while x != 0:
+            y = y * 10
+            y = y + (x % 10)
+            x = x // 10
+        return y
