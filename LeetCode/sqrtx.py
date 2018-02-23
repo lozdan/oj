db318957bad06f24cb7b930b614f65c130f29313
Daniel Lozano <lozanodaniel02@gmail.com>
2018-02-22 23:40:47 -0500

    add solution for sqrtx problem (LeetCode)

---
+++ LeetCode/sqrtx.py
@@ -0,0 +1,10 @@
+# author: Daniel Lozano
+# source: LeetCode ( https://www.leetcode.com )
+# problem name: Problems > Sqrt(x)
+# problem url: https://leetcode.com/problems/sqrtx/description/
+
+import math
+class Solution:
+    def mySqrt(self, x):
+        return int(math.sqrt(x))
+        
