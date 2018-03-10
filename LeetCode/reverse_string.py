d3bed068dcb8065f68e59281071464980e147311
Daniel Lozano <lozanodaniel02@gmail.com>
2018-03-10 14:49:41 -0500

    add solution for reverse_string problem (LeetCode)

---
+++ LeetCode/reverse_string.py
@@ -0,0 +1,9 @@
+# author: Daniel Lozano
+# source: LeetCode ( https://www.leetcode.com )
+# problem name: Problems > Reverse String
+# problem url: https://leetcode.com/problems/reverse-string/description/
+
+class Solution(object):
+    def reverseString(self, s):
+        return s[::-1]
+        
