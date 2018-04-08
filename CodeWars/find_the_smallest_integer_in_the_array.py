a57421b1ca4adc0b9df6c5088850ccb1b2f850a8
Daniel Lozano <lozanodaniel02@gmail.com>
2018-04-07 23:37:49 -0400

    add solution for find_the_smallest_integer_in_the_array

---
+++ CodeWars/find_the_smallest_integer_in_the_array.py
@@ -0,0 +1,7 @@
+# author: Daniel Lozano
+# source: CodeWars ( http://www.codewars.com )
+# problem name: Kata > Find the smallest integer in the array
+# problem url: https://www.codewars.com/kata/find-the-smallest-integer-in-the-array
+
+def findSmallestInt(arr):
+    return min(arr)
