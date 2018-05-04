89a4929709b763c465f9dd5ad50ea6aaa571073e
Daniel Lozano <lozanodaniel02@gmail.com>
2018-05-04 15:43:33 -0400

    add solution for Make Array Consecutive 2 problem (CodeFights)

---
+++ CodeFights/Make Array Consecutive 2.py
@@ -0,0 +1,10 @@
+
+
+def makeArrayConsecutive2(statues):
+    statues.sort()
+    res = 0
+    for i in range(statues[-1] - statues[0]):
+        if statues[i] + 1 != statues[i + 1]:
+            statues.insert(i+1, statues[i] + 1)
+            res += 1
+    return res
