0773cb76fb48d61573ce93a52e6c98fd16325b5b
Daniel Lozano <lozanodaniel02@gmail.com>
2018-05-07 11:03:30 -0400

    add solution for checkPalindrome problem (CodeFights)

---
+++ CodeFights/checkPalindrome.py
@@ -0,0 +1,8 @@
+# author: Daniel Lozano
+# source: CodeFights ( https://codefights.com )
+# problem name: checkPalindrome
+# problem url: https://codefights.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ/description
+
+def checkPalindrome(inputString):
+    return inputString == inputString[::-1]
+    
