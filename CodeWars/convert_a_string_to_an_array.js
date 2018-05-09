8cc0b468ddc028e23e96a8fcc2eb23751aaaaa60
Daniel Lozano <lozanodaniel02@gmail.com>
2018-05-09 18:42:34 -0400

    add solution for convert_a_string_to_an_array problem (CodeWars)

---
+++ CodeWars/convert_a_string_to_an_array.js
@@ -0,0 +1,13 @@
+/*
+author: Daniel Lozano
+source: CodeWars ( http://www.codewars.com )
+problem name: Kata > Convert a string to an array
+problem url: https://www.codewars.com/kata/convert-a-string-to-an-array/
+*/
+
+function stringToArray(string){
+
+	return string.split(" ");
+
+}
+
