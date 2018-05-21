7293307ec38535f7e104fc29293a2685628b0424
Daniel Lozano <lozanodaniel02@gmail.com>
2018-05-21 10:44:56 -0400

    add solution for fix_meassage problem (CodeFights)

---
+++ CodeFights/fix_message.py
@@ -0,0 +1,7 @@
+# author: Daniel Lozano
+# source: CodeFights ( https://codefights.com )
+# problem name: Fix Message
+# problem url: https://codefights.com/arcade/python-arcade/slithering-in-strings/Wmdqw8NBzcbqHSsw7
+
+def fixMessage(message):
+    return message.capitalize()
